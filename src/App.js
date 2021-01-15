0d1c8008714f5ad92dd147e2c956f41f5741660c
Zhengming Wang <zmwang622@gmail.com>
2021-01-14 23:58:09 -0500

    map gl init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
 function App() {
+  const [viewPort, setViewPort] = useState({
+    latitude: 40.441694,
+    longitude: -79.990086,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
+  })
   return (
-    <div className="App">
+    <div>
+      <ReactMapGL {...viewPort}></ReactMapGL>
     </div>
   );
 }
