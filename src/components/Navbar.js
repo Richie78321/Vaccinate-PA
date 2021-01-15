50d79ceb049f42a55240a829ed4c1d5b54f9951c
Zhengming Wang <zmwang622@gmail.com>
2021-01-15 02:49:01 -0500

    add  navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className="flex items-center justify-between flex-wrap p-6 bg-gray-900">
+
+        </nav>
+    )
+}
