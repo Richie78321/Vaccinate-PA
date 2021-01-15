bdc6f2554ab5ccc27c7ace0512e1140c3cb750d0
Zhengming Wang <zmwang622@gmail.com>
2021-01-15 03:45:43 -0500

    format popup

--- src/components/map/HospitalPopup.js
+++ src/components/map/HospitalPopup.js
@@ -10,13 +10,19 @@ export default function HospitalPopup(props) {
             props.setSelectedPark(null)
             }}
         >
-            <div className="">
+            <div className="container flex justify-between">
                 <h2
-                    className="font-bold text-lg"
+                    className="font-bold text-lg mr-2"
                 >
                     {props.selectedPark.properties.name}
                 </h2>
-                <p className="">{props.selectedPark.properties.address.street}</p>
+                <p className="ml-2 mt-0.5 text-sm text-gray-500">{props.selectedPark.properties.address.street}</p>
+            </div>
+            <div className="container flex justify_between">
+                { props.selectedPark.properties.has_doses ? 
+                    <span className="text-green-500 font-bold">Has doses</span>
+                    : <span className="text-red-500 font-bold">No doses</span>
+                }
             </div>
         </Popup>
     )
