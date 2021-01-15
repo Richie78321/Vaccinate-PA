b2029c20b029548ea8c979c4fd463786c975c9ad
Zhengming Wang <zmwang622@gmail.com>
2021-01-15 02:55:59 -0500

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Map from "./components/Map";
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <div>
+      <Navbar />
       <Map />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Navbar() {
     return (
-        <nav className="flex items-center justify-between flex-wrap p-6 bg-gray-900">
-
+        <nav className="flex items-center justify-between flex-wrap p-6 bg-black">
+            <div className="flex items-center flex-shrink-0 text-yellow-400 mr-6">
+                <a href="/" className="font-bold text-3xl">💬 VaccinatePA</a>
+            </div>
+            <div className="">
+                <a href="/" className="block mt-4 sm:inline-block text-yellow-400 sm:mt-0 hover:text-white mr-4">
+                    Contribute
+                </a>
+            </div>
         </nav>
     )
 }
