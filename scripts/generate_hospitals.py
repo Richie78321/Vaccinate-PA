7914fe4fb63381dedf4d0a6221c8eced432598af
Zhengming Wang <zmwang622@gmail.com>
2021-01-15 03:24:04 -0500

    add prettify phone number

--- scripts/generate_hospitals.py
+++ scripts/generate_hospitals.py
@@ -13,13 +13,17 @@ GENERATED_JSON = {"type": "FeatureCollection", "features":[]}
 hospitals = sh.get_worksheet(0)
 hospital_data = hospitals.get_all_records()
 
+def prettify_phone(num):
+    num = str(num)
+    return f"({num[0:3]}) {num[3:6]}-{num[6:]}"
+
 for i, hospital in enumerate(hospital_data):
     hosp_obj = {
         "type": "Feature", 
         "properties":{
             "name": hospital['Facility'],
             "address": hospital['Address'],
-            "phone": hospital['Phone_Num'],
+            "phone": prettify_phone(hospital['Phone_Num']),
             "has_doses": True if hospital['Has_Doses'].lower() == 'y' else False,
             "last_update": hospital['Last_Update_Time'],
         }, 
