3ce16a5d03378d70e13256004594f2848cb64aed
Zhengming Wang <zmwang622@gmail.com>
2021-01-19 11:56:46 -0500

    make locate button responsive

--- src/components/map/Locate.js
+++ src/components/map/Locate.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Locate(props) {
     return (
         <button 
-            className="bg-gray-300 py-1 px-6 my-2 text-center hover:bg-gray-800 hover:text-white active:bg-gray-600 active:text-white"
+            className="bg-gray-300 sm:py-1 px-3 text-xs sm:text-base sm:px-6 sm:my-2 text-center hover:bg-gray-800 hover:text-white active:bg-gray-600 active:text-white"
             onClick={() => {
                 navigator.geolocation.getCurrentPosition((position)=>{
                     props.panTo({
