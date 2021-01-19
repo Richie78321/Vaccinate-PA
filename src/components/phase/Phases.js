e804c76f9d1b08fa39aa576ca74709018a0dca13
Zhengming Wang <zmwang622@gmail.com>
2021-01-19 01:33:47 -0500

    responsive phase titles

--- src/components/phase/Phases.js
+++ src/components/phase/Phases.js
@@ -10,9 +10,9 @@ export default function Phases(props) {
     }
     
     return (
-        <div className="flex flex-col items-center mb-24 mt-36">
-            <h2 className="text-blue-900 text-4xl font-bold">Pennsylvania COVID-19 Vaccination Phases</h2>
-            <h2 className="text-blue-900 text-2xl font-bold mt-2">Currently In: <span className="bg-blue-300">Phase {props.phase}</span></h2>
+        <div className="bg-gray-200 flex flex-col items-center mb-24 mt-24 sm:mt-36">
+            <h2 className="text-blue-900 text-lg sm:text-4xl font-bold">Pennsylvania COVID-19 Vaccination Phases</h2>
+            <h2 className="text-blue-900 text-base sm:text-2xl font-bold mt-2">Currently In: <span className="bg-blue-300">Phase {props.phase}</span></h2>
             <Phasebar num={phaseToNum[props.phase]}/>
         </div>
     )
