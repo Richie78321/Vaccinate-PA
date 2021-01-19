4b448aa8cec8c04b5a462c528a8ab3e9671d0865
Zhengming Wang <zmwang622@gmail.com>
2021-01-19 01:26:03 -0500

    conditional render phases

--- src/components/phase/Phasebar.js
+++ src/components/phase/Phasebar.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Phasebar(props) {
     const phases = ["1A","1B","1C","2"]
     return (
-        <div className="flex border border-blue-900 my-2">
+        <div className="flex border border-blue-900 mb-2 mt-6">
             {
                 phases.map((phase, i)=>(
-                    (props.num <= i )
+                    (props.num >= i )
                     ?   <div key={phase} className="sm:w-60 sm:h-60 p-5 bg-blue-300 flex justify-center items-center font-bold text-2xl text-blue-900 transition duration-500 ease-in-out hover:bg-blue-900 hover:text-white">
                             Phase {phase}
                         </div>
-                    :   <div key={phase} className="sm:w-60 sm:h-60 p-5 bg-white flex justify-center items-center font-bold text-2xl text-gray-600 transition duration-500 ease-in-out hover:bg-blue-900 hover:text-white">
+                    :   <div key={phase} className="sm:w-60 sm:h-60 p-5 bg-white flex justify-center items-center font-bold text-2xl text-gray-400 transition duration-500 ease-in-out hover:bg-gray-500 hover:text-gray-200">
                             Phase {phase}
                         </div>
                 ))
