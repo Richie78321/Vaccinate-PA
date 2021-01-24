28aa383a7870ffacb6946c0638fbea3e3d11cf28
Richie Goulazian <rgoulazian@gmail.com>
2021-01-24 13:48:39 -0500

    Update main page text

--- src/pages/AirTableView.js
+++ src/pages/AirTableView.js
@@ -6,14 +6,7 @@ export default function AirTableView() {
     <div className="container-fluid mt-4">
       <h1>Pennsylvania COVID-19 Vaccine Availability</h1>
       <p>
-        As of January 19th, 2021, Pennsylvania has approved giving the COVID-19
-        vaccine to people age 65 and older. We are a volunteer team calling
-        hospitals and pharmacies to identify which facilities are currently
-        administering vaccines. This website will be updated daily to reflect
-        the latest information we are able to gather on facilities offering the
-        vaccine. This list is a work-in-progress and currently most facilities
-        with information are in Allegheny County. Additional Pennsylvania
-        counties will be added in the coming days.
+        As of January 19th, 2021, Pennsylvania has approved giving the COVID-19 vaccine to people age 65 and older. We are a volunteer team calling hospitals and pharmacies to identify which facilities are currently administering vaccines. This website will be updated daily to reflect the latest information we are able to gather. 
       </p>
       <p>
         Contact&nbsp;
