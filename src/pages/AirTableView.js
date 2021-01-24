7f4b2e2cfba240aeae7ab952aaf638b0271248b6
Richie78321 <Richie78321@users.noreply.github.com>
2021-01-24 18:50:00 +0000

    Apply Prettier formatting changes

--- src/pages/AirTableView.js
+++ src/pages/AirTableView.js
@@ -6,7 +6,11 @@ export default function AirTableView() {
     <div className="container-fluid mt-4">
       <h1>Pennsylvania COVID-19 Vaccine Availability</h1>
       <p>
-        As of January 19th, 2021, Pennsylvania has approved giving the COVID-19 vaccine to people age 65 and older. We are a volunteer team calling hospitals and pharmacies to identify which facilities are currently administering vaccines. This website will be updated daily to reflect the latest information we are able to gather. 
+        As of January 19th, 2021, Pennsylvania has approved giving the COVID-19
+        vaccine to people age 65 and older. We are a volunteer team calling
+        hospitals and pharmacies to identify which facilities are currently
+        administering vaccines. This website will be updated daily to reflect
+        the latest information we are able to gather.
       </p>
       <p>
         Contact&nbsp;
