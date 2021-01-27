397cf2de24a01e72ea4c2c1106e3768560b770f9
Richie Goulazian <rgoulazian@gmail.com>
2021-01-27 11:17:51 -0500

    Update logo and icon

--- src/components/SiteNavbar.js
+++ src/components/SiteNavbar.js
@@ -2,14 +2,15 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
+import logo from "../images/VaccinatePALogo.svg"
 
 export default function SiteNavbar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand as={Link} to="/">
-        <h2>
-          💬 <span className="text-warning font-weight-bold">VaccinatePA</span>
-        </h2>
+        <div>
+          <img alt="VaccinatePA" className="d-inline align-middle" src={logo} height="40" /><h2 className="d-inline align-middle mb-0 text-warning font-weight-bold">VaccinatePA</h2>
+        </div>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
