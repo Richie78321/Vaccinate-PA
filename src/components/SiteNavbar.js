ba079090b15cb3fa89908c84e321de0b4cd5efbd
Richie78321 <Richie78321@users.noreply.github.com>
2021-01-27 16:18:56 +0000

    Apply Prettier formatting changes

--- src/components/SiteNavbar.js
+++ src/components/SiteNavbar.js
@@ -2,14 +2,22 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
-import logo from "../images/VaccinatePALogo.svg"
+import logo from "../images/VaccinatePALogo.svg";
 
 export default function SiteNavbar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand as={Link} to="/">
         <div>
-          <img alt="VaccinatePA" className="d-inline align-middle" src={logo} height="40" /><h2 className="d-inline align-middle mb-0 text-warning font-weight-bold">VaccinatePA</h2>
+          <img
+            alt="VaccinatePA"
+            className="d-inline align-middle"
+            src={logo}
+            height="40"
+          />
+          <h2 className="d-inline align-middle mb-0 text-warning font-weight-bold">
+            VaccinatePA
+          </h2>
         </div>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
