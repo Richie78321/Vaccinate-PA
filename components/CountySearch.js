219ba574ae7fb2e7aae74c273b6f24bf534467f2
varughese <varughese@users.noreply.github.com>
2021-01-31 20:13:28 +0000

    Apply Prettier formatting changes

--- components/CountySearch.js
+++ components/CountySearch.js
@@ -3,41 +3,39 @@ import counties from "../content/counties";
 import { useRouter } from "next/router";
 
 export default function CountySearch({ searchRef }) {
-    const router = useRouter();
+  const router = useRouter();
 
-    const onButtonClick = () => {
-        // TODO: on clicking this button figure out closest county and go there
-        navigator.geolocation.getCurrentPosition(function (position) {
-            console.log("Latitude is :", position.coords.latitude);
-            console.log("Longitude is :", position.coords.longitude);
-        });
+  const onButtonClick = () => {
+    // TODO: on clicking this button figure out closest county and go there
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
 
-        if (searchRef.current) {
-            searchRef.current.focus();
-        }
-    };
+    if (searchRef.current) {
+      searchRef.current.focus();
+    }
+  };
 
-    return (
-        <div className="flex mt-3">
-            <div className="flex-1 county-search">
-                <Typeahead
-                    id="county-filter-selection"
-                    ref={searchRef}
-                    placeholder="Type your county..."
-                    options={counties}
-                    onChange={(selected) => {
-                        if (selected && selected.length > 0) {
-                            router.push(
-                                `counties/${selected[0].replace(" ", "_")}`
-                            );
-                        }
-                    }}
-                    clearButton
-                />
-            </div>
-            <button onClick={onButtonClick} className="ml-3 btn btn-info">
-                Find vaccine
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex mt-3">
+      <div className="flex-1 county-search">
+        <Typeahead
+          id="county-filter-selection"
+          ref={searchRef}
+          placeholder="Type your county..."
+          options={counties}
+          onChange={(selected) => {
+            if (selected && selected.length > 0) {
+              router.push(`counties/${selected[0].replace(" ", "_")}`);
+            }
+          }}
+          clearButton
+        />
+      </div>
+      <button onClick={onButtonClick} className="ml-3 btn btn-info">
+        Find vaccine
+      </button>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,27 @@ import CountySuggestions from "../components/CountySuggestion";
 import Layout from "../layouts/Layout";
 
 export default function Index() {
-    return (
-        <Layout title="Vaccine Availability">
-            <div className="container-fluid mt-4">
-                <h2 className="mt-4">
-                    Find a COVID-19 vaccine for yourself or a loved one
-                </h2>
-                <CountySuggestions />
-                <h4 className="mt-4">View all county information below:</h4>
-                <p className="mt-4 alert alert-secondary text-center">
-                    If you have a missing location to report, or think we have
-                    incorrect information,{" "}
-                    <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="https://airtable.com/shr7z01kc7h1ogP5R"
-                    >
-                        please let us know.
-                    </a>
-                </p>
-                <AirTableEmbed />
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout title="Vaccine Availability">
+      <div className="container-fluid mt-4">
+        <h2 className="mt-4">
+          Find a COVID-19 vaccine for yourself or a loved one
+        </h2>
+        <CountySuggestions />
+        <h4 className="mt-4">View all county information below:</h4>
+        <p className="mt-4 alert alert-secondary text-center">
+          If you have a missing location to report, or think we have incorrect
+          information,{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://airtable.com/shr7z01kc7h1ogP5R"
+          >
+            please let us know.
+          </a>
+        </p>
+        <AirTableEmbed />
+      </div>
+    </Layout>
+  );
 }
