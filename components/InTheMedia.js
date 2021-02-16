82852ca2d3e4de6e40b144b8cc92dfddf4c30c0a
Richie78321 <Richie78321@users.noreply.github.com>
2021-02-16 02:56:15 +0000

    Apply Prettier formatting changes

--- components/InTheMedia.js
+++ components/InTheMedia.js
@@ -1,50 +1,61 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function InTheMedia() {
   const media = [
     {
-      href: 'https://www.wsj.com/articles/covid-19-vaccine-confusion-leads-volunteers-to-create-online-guides-11612002601',
-      title: 'Covid-19 Vaccine Confusion Leads Volunteers to Create Online Guides',
-      imagePath: '/images/WSJ.jpg',
-      imageAlt: 'Wall Street Journal',
+      href:
+        "https://www.wsj.com/articles/covid-19-vaccine-confusion-leads-volunteers-to-create-online-guides-11612002601",
+      title:
+        "Covid-19 Vaccine Confusion Leads Volunteers to Create Online Guides",
+      imagePath: "/images/WSJ.jpg",
+      imageAlt: "Wall Street Journal",
     },
     {
-      href: 'https://www.wtae.com/article/pitt-students-volunteer-to-create-website-to-help-pennsylvanians-find-covid-19-vaccines/35426401',
-      title: 'Pitt students volunteer to create website to help Pennsylvanians find COVID-19 vaccines',
-      imagePath: '/images/ActionNews.jpg',
-      imageAlt: 'Pittsburgh\'s Action News 4',
+      href:
+        "https://www.wtae.com/article/pitt-students-volunteer-to-create-website-to-help-pennsylvanians-find-covid-19-vaccines/35426401",
+      title:
+        "Pitt students volunteer to create website to help Pennsylvanians find COVID-19 vaccines",
+      imagePath: "/images/ActionNews.jpg",
+      imageAlt: "Pittsburgh's Action News 4",
     },
     {
-      href: 'https://www.post-gazette.com/local/region/2021/02/07/Stressed-Pennsylvanians-COVID-19-vaccination/stories/202102070138',
-      title: "‘It’s ridiculous’: Stressed Pennsylvanians craving easier path to COVID-19 vaccination",
-      imagePath: '/images/PittsburghPostGazette.jpg',
-      immageAlt: 'Pittsburgh Post Gazette',
+      href:
+        "https://www.post-gazette.com/local/region/2021/02/07/Stressed-Pennsylvanians-COVID-19-vaccination/stories/202102070138",
+      title:
+        "‘It’s ridiculous’: Stressed Pennsylvanians craving easier path to COVID-19 vaccination",
+      imagePath: "/images/PittsburghPostGazette.jpg",
+      immageAlt: "Pittsburgh Post Gazette",
     },
     {
-      href: 'https://www.abc27.com/news/health/coronavirus/coronavirus-pennsylvania/i-need-to-get-involved-volunteer-run-vaccinatepa-org-helps-pennsylvanians-find-appointments/',
-      title: '“I need to get involved” – Volunteer-run VaccinatePA.org helps Pennsylvanians find appointments',
-      imagePath: '/images/ABC27.jpg',
-      imageAlt: 'ABC 27 Harrisburg'
+      href:
+        "https://www.abc27.com/news/health/coronavirus/coronavirus-pennsylvania/i-need-to-get-involved-volunteer-run-vaccinatepa-org-helps-pennsylvanians-find-appointments/",
+      title:
+        "“I need to get involved” – Volunteer-run VaccinatePA.org helps Pennsylvanians find appointments",
+      imagePath: "/images/ABC27.jpg",
+      imageAlt: "ABC 27 Harrisburg",
     },
     {
-      href: 'https://www.pittsburghmagazine.com/college-students-may-have-a-better-way-to-find-the-covid-vaccine/',
-      title: 'College Students May Have a Better Way to Find the COVID Vaccine',
-      imagePath: '/images/PittsburghMagazine.jpg',
-      imageAlt: 'Pittsburgh Magazine'
+      href:
+        "https://www.pittsburghmagazine.com/college-students-may-have-a-better-way-to-find-the-covid-vaccine/",
+      title: "College Students May Have a Better Way to Find the COVID Vaccine",
+      imagePath: "/images/PittsburghMagazine.jpg",
+      imageAlt: "Pittsburgh Magazine",
     },
     {
-      href: 'https://pittsburgh.cbslocal.com/2021/02/04/college-students-create-website-coronavirus-vaccine-website/',
-      title: 'COVID-19 In Pennsylvania: 3 College Students Create Website For Finding Vaccine',
-      imagePath: '/images/KDKA.jpg',
-      imageAlt: 'KDKA CBS Pittsburgh',
-    }
+      href:
+        "https://pittsburgh.cbslocal.com/2021/02/04/college-students-create-website-coronavirus-vaccine-website/",
+      title:
+        "COVID-19 In Pennsylvania: 3 College Students Create Website For Finding Vaccine",
+      imagePath: "/images/KDKA.jpg",
+      imageAlt: "KDKA CBS Pittsburgh",
+    },
   ];
 
   return (
     <div className="container mt-5">
       <h4 className="text-center mb-3">In the Media</h4>
       <div className="row align-items-center mb-3 px-4">
-        {media.map((media_instance) => 
+        {media.map((media_instance) => (
           <div key={media_instance.href} className="col-12 col-sm-6 col-md-4">
             <a href={media_instance.href} target="_blank" rel="noreferrer">
               <div className="d-flex flex-column align-items-center">
@@ -61,7 +72,8 @@ export default function InTheMedia() {
                 </p>
               </div>
             </a>
-          </div>)}
+          </div>
+        ))}
       </div>
     </div>
   );
