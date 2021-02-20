f978e91c73311333785bda666f0d7f900df0d456
Richie Goulazian <rgoulazian@gmail.com>
2021-02-20 18:50:50 -0500

    Remove Google optimize for now

--- pages/_app.js
+++ pages/_app.js
@@ -38,13 +38,6 @@ function MyApp({ Component, pageProps }) {
               `,
           }}
         />
-        {/* Google Optimize */}
-        <script async src="https://www.googleoptimize.com/optimize.js?id=OPT-KF854K7"></script>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
         <link
           rel="icon"
           type="image/png"
