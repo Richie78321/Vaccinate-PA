4c9c7bba3a2581c1e419ad8de93483bfebebc073
Richie Goulazian <rgoulazian@gmail.com>
2021-02-22 16:22:09 -0500

    Add distance-based location query

--- utils/Data.js
+++ utils/Data.js
@@ -4,6 +4,8 @@ import haversine from "haversine";
 
 const OUTDATED_DAYS_THRESHOLD = 3;
 
+const LOCATION_MILE_THRESHOLDS = [5, 10, 25, 50, 75, 100];
+
 const backupDataCache = new NodeCache({
   deleteOnExpire: false,
   stdTTL: 0,
@@ -102,6 +104,8 @@ export async function getCountyLinks(county) {
   }
 }
 
+// TODO : Both this and the lat long location function are in need of a serious
+// refactor WRT the location groups and categories.
 export function getCountyLocations(county) {
   return cacheData(
     county,
@@ -192,3 +196,82 @@ export function getCountyLocations(county) {
   );
 }
 
+export function getLatLongLocations(latitude, longitude) {
+  // Could employ some sort of lat-long discretization in the future if further
+  // caching is necessary. Definitely overkill for now.
+  return cacheData(
+    `${latitude}-${longitude}`,
+    async () => {
+      // Uses hours instead of days because AirTable's rounding for days is too
+      // lenient and some display as "4 days ago" on frontend.
+      const allAvailability = (await Airtable.base("appdsheneg5ii1EnQ")("Locations").select({
+        filterByFormula: `
+          AND(
+            {Latest report},
+            DATETIME_DIFF(TODAY(), {Latest report}, 'hours') <= ${OUTDATED_DAYS_THRESHOLD * 24},
+            {Latitude},
+            {Longitude},
+            NOT({Vaccines available?} = 'No')
+          )
+        `,
+        sort: [
+          {
+            field: "Latest report",
+            direction: "desc",
+          },
+        ]
+      }).all())
+          .map((record) => record._rawJson);
+
+      for (let i = 0; i < allAvailability.length; i++) {
+        allAvailability[i].availabilityStatus = getAvailabilityStatus(
+          allAvailability[i].fields["Vaccines available?"]
+        );
+      }
+      
+      const start = {latitude, longitude};
+      for (let i = 0; i < allAvailability.length; i++) {
+        allAvailability[i].distanceMiles = haversine(start, {
+            latitude: allAvailability[i].fields.Latitude,
+            longitude: allAvailability[i].fields.Longitude,
+          },
+          { unit: 'mile' },
+        );
+      }
+      
+      const locationDistanceBuckets = [];
+      for (let i = 0; i < LOCATION_MILE_THRESHOLDS.length; i++) {
+        let locationsWithinBucket;
+        if (i == 0) {
+          locationsWithinBucket = allAvailability.filter((location) => location.distanceMiles <= LOCATION_MILE_THRESHOLDS[i]);
+        } else {
+          locationsWithinBucket = allAvailability.filter((location) => 
+              location.distanceMiles <= LOCATION_MILE_THRESHOLDS[i] && location.distanceMiles > LOCATION_MILE_THRESHOLDS[i - 1]);
+        }
+
+        locationDistanceBuckets.push({
+          mileThreshold: LOCATION_MILE_THRESHOLDS[i],
+          locations: {
+            availableWaitlist: locationsWithinBucket.filter(
+              (location) =>
+                location.availabilityStatus.value ===
+                AVAILABILITY_STATUS.WAITLIST.value
+            ),
+            availableAppointment: locationsWithinBucket.filter(
+              (location) =>
+                location.availabilityStatus.value ===
+                AVAILABILITY_STATUS.APPOINTMENT.value
+            ),
+            availableWalkIn: locationsWithinBucket.filter(
+              (location) =>
+                location.availabilityStatus.value ===
+                AVAILABILITY_STATUS.WALK_IN.value
+            ),
+          },
+        });
+      }
+
+      return locationDistanceBuckets;
+    }
+  );
+}
