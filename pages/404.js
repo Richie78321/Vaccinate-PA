01a6c1409e93be75390eead5ebb1f5901d7a5865
GideonShils <GideonShils@users.noreply.github.com>
2021-02-22 01:05:42 +0000

    Apply Prettier formatting changes

--- pages/404.js
+++ pages/404.js
@@ -6,17 +6,10 @@ export default function FourOhFour() {
   return (
     <Layout title="Page Not Found">
       <div className="m-5 text-center">
-        <h1>
-          Page Not Found
-        </h1>
-        <p>
-          Sorry, this page doesn't seem to exist.
-        </p>
+        <h1>Page Not Found</h1>
+        <p>Sorry, this page doesn't seem to exist.</p>
         <Link href="/">
-          <Button
-            variant="warning"
-            className="rounded-pill my-2"
-          >
+          <Button variant="warning" className="rounded-pill my-2">
             Go Back Home
           </Button>
         </Link>
