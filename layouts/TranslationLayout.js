e16474304ecb32bb9c27610c2757f686b555e058
Richie Goulazian <rgoulazian@gmail.com>
2021-03-02 18:51:22 -0500

    Move translation options to component

--- layouts/TranslationLayout.js
+++ layouts/TranslationLayout.js
@@ -1,37 +1,12 @@
 import Layout from "./Layout";
-import { FaAngleDoubleDown } from "react-icons/fa";
+import TranslationOptions from "../components/TranslationOptions";
 
 export default function TranslationLayout({ children, ...otherProps }) {
   return (
     <Layout {...otherProps}>
       {/* Page Translations */}
       <div className="mt-3 text-center text-sm-right mr-sm-4">
-        <a
-          href="?lang=en#googtrans(en|en)"
-          className="notranslate"
-          data-lang="en"
-        >
-          English
-        </a>
-        {" · "}
-        <a
-          href="?lang=es#googtrans(en|es)"
-          className="notranslate"
-          data-lang="es"
-        >
-          Español
-        </a>
-        {" · "}
-        <a
-          href="?lang=zh#googtrans(en|zh-CN)"
-          className="notranslate"
-          data-lang="zh"
-        >
-          简体中文
-        </a>
-        <a href="#google_translate_element" className="ml-1">
-          <FaAngleDoubleDown />
-        </a>
+        <TranslationOptions />
       </div>
       {children}
     </Layout>
