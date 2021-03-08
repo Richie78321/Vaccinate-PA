a1f9b0b0497c5220570c2a29abb6ef0b5fc4ccca
Richie Goulazian <rgoulazian@gmail.com>
2021-03-08 15:17:59 -0500

    Update UI for translation options

--- components/TranslationOptions.js
+++ components/TranslationOptions.js
@@ -1,29 +1,44 @@
+import { IoIosGlobe } from "react-icons/io";
+import { useState, useCallback } from "react";
+
 export default function TranslationOptions() {
-  return (
-    <>
-      <a
-        href="?lang=en#googtrans(en|en)"
-        className="notranslate"
-        data-lang="en"
-      >
-        English
+  const [menuExpanded, setMenuExpanded] = useState(false);
+  const onClickCallback = useCallback(() => setMenuExpanded(true), [setMenuExpanded]);
+
+  if (!menuExpanded) {
+    return (
+      <a href="#" onClick={onClickCallback}>
+        <span className="align-middle">Translate</span>
+        <IoIosGlobe size="1.25em" style={{ marginLeft: "2px" }} />
       </a>
-      {" · "}
-      <a
-        href="?lang=es#googtrans(en|es)"
-        className="notranslate"
-        data-lang="es"
-      >
-        Español
-      </a>
-      {" · "}
-      <a
-        href="?lang=zh#googtrans(en|zh-CN)"
-        className="notranslate"
-        data-lang="zh"
-      >
-        简体中文
-      </a>
-    </>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <a
+          href="?lang=en#googtrans(en|en)"
+          className="notranslate"
+          data-lang="en"
+        >
+          English
+        </a>
+        {" · "}
+        <a
+          href="?lang=es#googtrans(en|es)"
+          className="notranslate"
+          data-lang="es"
+        >
+          Español
+        </a>
+        {" · "}
+        <a
+          href="?lang=zh#googtrans(en|zh-CN)"
+          className="notranslate"
+          data-lang="zh"
+        >
+          简体中文
+        </a>
+      </div>
+    );  
+  }
 }
