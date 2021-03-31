c15bc11f7f10f50abac478c965a41f7d5ad338c2
Richie78321 <Richie78321@users.noreply.github.com>
2021-03-31 00:44:23 +0000

    Apply Prettier formatting changes

--- components/RealtimeCountyLocations.js
+++ components/RealtimeCountyLocations.js
@@ -87,7 +87,10 @@ export default class RealtimeCountyLocations extends Component {
         </h3>
         <p className="mb-2" style={{ lineHeight: "100%", marginTop: "4px" }}>
           <small>
-            Realtime availability is updated every minute. <Link href="/additional-resources#real-time-availability">Learn more.</Link>
+            Realtime availability is updated every minute.{" "}
+            <Link href="/additional-resources#real-time-availability">
+              Learn more.
+            </Link>
           </small>
         </p>
         <div className="d-flex flex-row">
