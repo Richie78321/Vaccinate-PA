2c6453a8cffc0049b106aa155d6e831706279c7a
Mathew Varughese <mat.varughese3@gmail.com>
2021-04-04 23:09:24 -0400

    Update RealtimeLocationCard.js

--- components/RealtimeLocationCard.js
+++ components/RealtimeLocationCard.js
@@ -139,7 +139,7 @@ const AppointmentDetail = ({ location }) => {
           </div>
           {location.properties.postal_code && websiteUrl && (
             <small>
-              <a href={websiteUrl}>Click here</a> to schedule an appointmment
+              <a href={websiteUrl}>Click here</a> to schedule an appointment
               and enter the zip code <b>{location.properties.postal_code}</b>{" "}
               when searching.{" "}
             </small>
