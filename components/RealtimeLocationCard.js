33498254b02b6d3e416d486527fc95cc3398d0f7
varughese <mat.varughese3@gmail.com>
2021-04-04 22:34:34 -0400

    group

--- components/RealtimeLocationCard.js
+++ components/RealtimeLocationCard.js
@@ -2,67 +2,106 @@ import moment from "moment";
 import { FaCalendarAlt, FaExternalLinkAlt } from "react-icons/fa";
 import { BsInfoCircle } from "react-icons/bs";
 
-// Smelly af but works for now.
+// Smelly af but works for now. <- lol
 const brandInstructions = {
-  walgreens: (location) => (
-    <>
-      <span className="text-nowrap">
-        <a
-          href="https://drive.google.com/file/d/1aZmz5lYQ2eh2PeALw1FrqPSJjOYJStK-/view?usp=sharing"
-          target="_blank"
-          rel="noreferrer"
-        >
-          View More <FaExternalLinkAlt size=".75em" />
-        </a>
-      </span>
-      {" | "}
-      <ZipCodeInstructions location={location} />
-    </>
-  ),
-  rite_aid: (location) => (
-    <>
-      <span className="text-nowrap">
-        <a
-          href="https://drive.google.com/file/d/1h2PRp_kNFGGuieq9KIAwIkGYB1ohJYP5/view?usp=sharing"
-          target="_blank"
-          rel="noreferrer"
-        >
-          View More <FaExternalLinkAlt size=".75em" />
-        </a>
-      </span>
-      {" | "}
-      <ZipCodeInstructions location={location} />
-    </>
-  ),
-  weis: (location) => (
-    <>
-      <span className="text-nowrap">
-        <a
-          href="https://drive.google.com/file/d/1L4gCp3cke2DmIiU-014GrGyhopre2L_M/view?usp=sharing"
-          target="_blank"
-          rel="noreferrer"
-        >
-          View More <FaExternalLinkAlt size=".75em" />
-        </a>
-      </span>
-      {" | "}
-      <ZipCodeInstructions location={location} />
-    </>
-  ),
-  wegmans: (location) => <ZipCodeInstructions location={location} />,
+    walgreens: () => (
+        <>
+            <span className="text-nowrap">
+                <a href="" target="_blank" rel="noreferrer">
+                    View More <FaExternalLinkAlt size=".75em" />
+                </a>
+            </span>
+        </>
+    ),
+    rite_aid: () => (
+        <>
+            <span className="text-nowrap">
+                <a
+                    href="https://drive.google.com/file/d/1h2PRp_kNFGGuieq9KIAwIkGYB1ohJYP5/view?usp=sharing"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    View More <FaExternalLinkAlt size=".75em" />
+                </a>
+            </span>
+        </>
+    ),
+    weis: () => (
+        <>
+            <span className="text-nowrap">
+                <a
+                    href="https://drive.google.com/file/d/1L4gCp3cke2DmIiU-014GrGyhopre2L_M/view?usp=sharing"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    View More <FaExternalLinkAlt size=".75em" />
+                </a>
+            </span>
+        </>
+    ),
+};
+
+const PROVIDER_TO_METADATA = {
+    albertsons: {
+        name: "ACME",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/8/80/Logo_acme.svg",
+    },
+    walgreens: {
+        name: "Walgreens",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/6/65/Walgreens_Logo.svg",
+        instructions:
+            "https://drive.google.com/file/d/1aZmz5lYQ2eh2PeALw1FrqPSJjOYJStK-/view?usp=sharing",
+    },
+    cvs: {
+        name: "CVS",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/1/1b/CVS_Pharmacy_Alt_Logo.svg",
+    },
+    rite_aid: {
+        name: "Rite Aid",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/6/6b/Rite_Aid.svg",
+        instructions:
+            "https://drive.google.com/file/d/1h2PRp_kNFGGuieq9KIAwIkGYB1ohJYP5/view?usp=sharing",
+    },
+    sams_club: {
+        name: "Sams Club",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/e/e7/Sams_Club.svg",
+    },
+    walmart: {
+        name: "Walmart",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/c/ca/Walmart_logo.svg",
+    },
+    wegmans: {
+        name: "Wegmans",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/d/de/WegmansLogo.svg",
+    },
+    weis: {
+        name: "Weis",
+        image:
+            "https://upload.wikimedia.org/wikipedia/commons/f/f3/Weis_Markets_logo.svg",
+        instructions:
+            "https://drive.google.com/file/d/1L4gCp3cke2DmIiU-014GrGyhopre2L_M/view?usp=sharing",
+    },
 };
 
 function ZipCodeInstructions({ location }) {
-  if (!location.properties.postal_code) {
-    return null;
-  }
+    if (!location.properties.postal_code) {
+        return null;
+    }
 
-  return (
-    <span>
-      Enter the zip code <b>{location.properties.postal_code}</b> when searching
-      for appointments. Look for the location with a matching address.
-    </span>
-  );
+    return (
+        <span>
+            Enter the zip code <b>{location.properties.postal_code}</b> when
+            searching for appointments. Look for the location with a matching
+            address.
+        </span>
+    );
 }
 
 // function toTitleCase(str) {
@@ -79,125 +118,193 @@ function ZipCodeInstructions({ location }) {
 //   }
 // }
 
-export default function RealtimeLocationCard({ location }) {
-  const website = location?.properties?.url;
-  const address = `${location.properties.address}, ${location.properties.city} ${location.properties.postal_code}`;
-  const latestReportTimeText = location.properties.appointments_last_fetched
-    ? moment(location.properties.appointments_last_fetched).fromNow()
-    : null;
-  const name = location.properties.provider_brand_name
-    ? location.properties.provider_brand_name
-    : location.properties.name;
-
-  const appointments = location.properties.appointments;
-  // if (appointments) {
-  //   appointments.forEach((appointment) => {
-  //     formatAppointment(appointment);
-  //   });
-  // }
-
-  let instructions = null;
-  if (
-    location.properties.provider &&
-    brandInstructions[location.properties.provider]
-  ) {
-    instructions = brandInstructions[location.properties.provider](location);
-  }
-
-  const distanceMiles = location.distanceMiles;
-
-  return (
-    <>
-      <div className="location-card card">
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <div className="row">
-              <div className="col-auto">
-                <h5 className="mb-0 card-title text-truncate">{name}</h5>
-                <p className="my-0 text-truncate">
-                  {website ? (
-                    <>
-                      <a href={website} target="_blank" rel="noreferrer">
+const AppointmentDetail = ({ location }) => {
+    const appointments = location.properties?.appointments;
+    const address = `${location.properties.address}, ${location.properties.city} ${location.properties.postal_code}`;
+    const distanceMiles = location.distanceMiles;
+    const websiteUrl = location?.properties?.url;
+    return (
+        <li className="list-group-item">
+            {address ? (
+                <>
+                    <div>
                         <small>
-                          Schedule Appointment{" "}
-                          <FaExternalLinkAlt size=".85em" />
+                            <u>{address}</u>{" "}
+                            <a
+                                target="_blank"
+                                rel="noreferrer"
+                                href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(
+                                    address
+                                )}`}
+                            >
+                                <FaExternalLinkAlt size=".85em" />
+                            </a>
                         </small>
-                      </a>
-                      <span className="text-muted d-none d-sm-inline">
-                        {" | "}
-                      </span>
-                      <br className="d-block d-sm-none" />
-                    </>
-                  ) : null}
-                  {distanceMiles ? (
-                    <>
-                      <small>~{Math.round(distanceMiles)} miles away</small>
-                      <span className="text-muted">{" | "}</span>
-                    </>
-                  ) : null}
-                  {address ? (
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(
-                        address
-                      )}`}
-                    >
-                      <small>{address}</small>
-                    </a>
-                  ) : null}
-                </p>
-              </div>
-              {latestReportTimeText ? (
-                <div className="ml-auto col-auto text-right">
-                  <span className="badge badge-pill badge-light font-weight-normal text-wrap">
-                    Updated {latestReportTimeText}
-                  </span>
-                </div>
-              ) : null}
-            </div>
-          </li>
-          <li className="list-group-item">
-            <span className="text-success font-weight-bold">
-              <FaCalendarAlt size="1.25em" className="mr-1" />{" "}
-              <span className="align-middle">
-                Vaccines available with appointment
-              </span>
-            </span>
-          </li>
-          {instructions ? (
-            <li className="list-group-item">
-              <span className="text-black">
-                <BsInfoCircle size="1.25em" className="mr-1" />{" "}
-                <span className="align-middle">
-                  <span className="font-weight-bold">Scheduling Tips:</span>{" "}
-                  {instructions}
-                </span>
-              </span>
-            </li>
-          ) : null}
-          {appointments && appointments.length > 0 ? (
-            <li className="list-group-item">
-              {appointments.map((appointment, index) => (
-                <div key={index}>
-                  <span className="badge badge-light mr-2 font-weight-normal">
-                    {appointment.num} Appointments
-                  </span>
-                  {moment.utc(appointment.time).format("dddd, MMMM Do")}
-                  {/* {appointment.types
+                    </div>
+                    {location.properties.postal_code && websiteUrl && (
+                        <small>
+                            <a href={websiteUrl}>Click here</a> to schedule an
+                            appointmment and enter the zip code{" "}
+                            <b>{location.properties.postal_code}</b> when
+                            searching.{" "}
+                        </small>
+                    )}
+                    <div>
+                        <small></small>
+                    </div>
+                </>
+            ) : null}
+            {appointments && appointments.length > 0 ? (
+                <>
+                    {appointments.map((appointment, index) => (
+                        <div key={index}>
+                            <small>
+                                <span className="badge badge-light mr-2 font-weight-normal">
+                                    {appointment.num} Appointments
+                                </span>
+                                {moment
+                                    .utc(appointment.time)
+                                    .format("dddd, MMMM Do")}
+                                {/* {appointment.types
                       ? ": " + appointment.types.join(" & ")
                       : null} */}
-                </div>
-              ))}
-            </li>
-          ) : null}
-        </ul>
-      </div>
-      <style jsx>{`
-        .location-card {
-          box-shadow: 1px 1px 20px 2px rgb(0 0 0 / 10%);
-        }
-      `}</style>
-    </>
-  );
+                            </small>
+                        </div>
+                    ))}
+                </>
+            ) : null}
+            <p className="my-0 text-truncate">
+                {distanceMiles ? (
+                    <>
+                        <small>~{Math.round(distanceMiles)} miles away</small>
+                        <span className="text-muted">{" | "}</span>
+                    </>
+                ) : null}
+            </p>
+        </li>
+    );
+};
+
+export default function RealtimeLocationCard({ providerId, locations }) {
+    const firstLocation = locations[0];
+    const website = firstLocation?.properties?.url;
+    const latestReportTimeText = firstLocation.properties
+        .appointments_last_fetched
+        ? moment(firstLocation.properties.appointments_last_fetched).fromNow()
+        : null;
+    const name = firstLocation.properties.provider_brand_name
+        ? firstLocation.properties.provider_brand_name
+        : firstLocation.properties.name;
+
+    // if (appointments) {
+    //   appointments.forEach((appointment) => {
+    //     formatAppointment(appointment);
+    //   });
+    // }
+
+    let instructionsLink = null;
+    if (brandInstructions[providerId]) {
+        instructionsLink = PROVIDER_TO_METADATA[providerId].instructions;
+    }
+
+    const logoUrl = PROVIDER_TO_METADATA[providerId].image;
+
+    return (
+        <>
+            <div className="location-card card">
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <div className="row">
+                            <div className="col-auto title-section">
+                                <div>
+                                    <h5 className="mb-0 card-title text-truncate">
+                                        {name}
+                                    </h5>
+                                    {website ? (
+                                        <a
+                                            href={website}
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <small>
+                                                Schedule Appointment{" "}
+                                                <FaExternalLinkAlt size=".85em" />
+                                            </small>
+                                        </a>
+                                    ) : null}
+                                </div>
+                                <div className="realtime-right-card flex">
+                                    {latestReportTimeText ? (
+                                        <span className="ml-3 mb-2 badge badge-pill badge-light font-weight-normal text-wrap">
+                                            Updated {latestReportTimeText}
+                                        </span>
+                                    ) : null}
+                                    <img
+                                        className="provider-logo"
+                                        src={logoUrl}
+                                    ></img>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="text-success font-weight-bold">
+                            <FaCalendarAlt size="1.25em" className="mr-1" />{" "}
+                            <span className="align-middle">
+                                Vaccines available with appointment
+                            </span>
+                        </span>
+                    </li>
+                    {instructionsLink ? (
+                        <li className="list-group-item">
+                            <span className="text-black">
+                                <BsInfoCircle size="1.25em" className="mr-1" />{" "}
+                                <span className="align-middle">
+                                    <span className="font-weight-bold">
+                                        Scheduling Tips:
+                                    </span>{" "}
+                                    Enter the zip code of the location when
+                                    searching for appointments. Look for the
+                                    location with a matching address.{" "}
+                                    <a
+                                        href={instructionsLink}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        View More{" "}
+                                        <FaExternalLinkAlt size=".75em" />
+                                    </a>
+                                </span>
+                            </span>
+                        </li>
+                    ) : null}
+
+                    {locations.map((location) => (
+                        <AppointmentDetail location={location} />
+                    ))}
+                </ul>
+            </div>
+            <style jsx>{`
+                .title-section {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    width: 100%;
+                }
+                .realtime-right-card {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: right;
+                    align-items: flex-end;
+                }
+                .location-card {
+                    box-shadow: 1px 1px 20px 2px rgb(0 0 0 / 10%);
+                }
+                .provider-logo {
+                    height: 40px;
+                    max-width: 170px;
+                }
+            `}</style>
+        </>
+    );
 }
--- components/RealtimeLocations.js
+++ components/RealtimeLocations.js
@@ -8,106 +8,137 @@ import Link from "next/link";
 const DEFAULT_REFRESH_TIME = 60000; // One minute
 
 export default class RealtimeLocations extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      lastUpdated: null,
-      locations: [],
-    };
+        this.state = {
+            lastUpdated: null,
+            locations: [],
+        };
 
-    /**
-     * Controller used to abort fetch requests if the React component is
-     * unmounted during a request:
-     * https://stackoverflow.com/questions/31061838/how-do-i-cancel-an-http-fetch-request
-     */
-    this.abortController = new AbortController();
-  }
+        /**
+         * Controller used to abort fetch requests if the React component is
+         * unmounted during a request:
+         * https://stackoverflow.com/questions/31061838/how-do-i-cancel-an-http-fetch-request
+         */
+        this.abortController = new AbortController();
+    }
 
-  componentDidMount() {
-    this.fetchUpdatedLocations();
-    this.reloadInterval = setInterval(
-      this.fetchUpdatedLocations.bind(this),
-      this.props.refreshSeconds
-        ? this.props.refreshSeconds * 1000
-        : DEFAULT_REFRESH_TIME
-    );
-  }
+    componentDidMount() {
+        this.fetchUpdatedLocations();
+        this.reloadInterval = setInterval(
+            this.fetchUpdatedLocations.bind(this),
+            this.props.refreshSeconds
+                ? this.props.refreshSeconds * 1000
+                : DEFAULT_REFRESH_TIME
+        );
+    }
 
-  componentWillUnmount() {
-    // Abort ongoing fetch requests if there are any.
-    this.abortController.abort();
-    clearInterval(this.reloadInterval);
-  }
+    componentWillUnmount() {
+        // Abort ongoing fetch requests if there are any.
+        this.abortController.abort();
+        clearInterval(this.reloadInterval);
+    }
 
-  fetchUpdatedLocations() {
-    fetch(this.props.apiURL, {
-      signal: this.abortController.signal,
-    })
-      .then((resp) => resp.json())
-      .then((resp) => {
-        if (resp?.locations) {
-          const updateTime = Date.now();
-          this.setState({
-            lastUpdated: updateTime,
-            locations: resp.locations,
-          });
+    fetchUpdatedLocations() {
+        fetch(this.props.apiURL, {
+            signal: this.abortController.signal,
+        })
+            .then((resp) => resp.json())
+            .then((resp) => {
+                if (resp?.locations) {
+                    const updateTime = Date.now();
+                    this.setState({
+                        lastUpdated: updateTime,
+                        locations: resp.locations,
+                    });
 
-          if (resp.locations.length > 0) {
-            this.props.updateLatestReportTime(
-              resp.locations[0].properties?.appointments_last_fetched
-            );
-          }
+                    if (resp.locations.length > 0) {
+                        this.props.updateLatestReportTime(
+                            resp.locations[0].properties
+                                ?.appointments_last_fetched
+                        );
+                    }
+                }
+            })
+            .catch((err) => {}); // TODO : Consider implications of failed request.
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.apiURL !== this.props.apiURL) {
+            this.fetchUpdatedLocations();
         }
-      })
-      .catch((err) => {}); // TODO : Consider implications of failed request.
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevProps.apiURL !== this.props.apiURL) {
-      this.fetchUpdatedLocations();
-    }
-  }
-
-  render() {
-    const { lastUpdated, locations } = this.state;
-
-    if (!lastUpdated) {
-      return (
-        <div className="text-center my-2">
-          <BeatLoader size="0.5em" /> Loading Realtime Data...
-        </div>
-      );
     }
 
-    return (
-      <div className={locations.length <= 0 ? "mb-0" : "mb-2"}>
-        <h3 className="font-weight-normal mb-0">
-          <u>Realtime availability:</u>
-        </h3>
-        <p className="mb-2" style={{ lineHeight: "100%", marginTop: "4px" }}>
-          <small>
-            Realtime availability is updated every minute.{" "}
-            <Link href="/about-us#where-does-real-time-availability-information-come-from-">
-              Learn more
-            </Link>
-          </small>
-        </p>
-        <div className="d-flex flex-row">
-          <div>
-            <ImSpinner2 className="rotating" />
-          </div>
-          <div className="ml-1" style={{ fontSize: "110%" }}>
-            Last {locations.length <= 0 ? "checked" : "updated"}{" "}
-            {moment(lastUpdated).format("h:mma")}
-          </div>
-        </div>
-        {locations.map((location) => (
-          <div key={location.properties.id} className="my-3">
-            <RealtimeLocationCard location={location} />
-          </div>
-        ))}
-      </div>
-    );
-  }
+    render() {
+        const { lastUpdated, locations } = this.state;
+
+        if (!lastUpdated) {
+            return (
+                <div className="text-center my-2">
+                    <BeatLoader size="0.5em" /> Loading Realtime Data...
+                </div>
+            );
+        }
+
+        // How to get
+        // list of pharmacy IDs:
+        // curl https://www.vaccinespotter.org/api/v0/states/PA.json | jq '.metadata.provider_brands | [.[] | .provider_id]'
+        const locationsByPharmacy = locations.reduce((acc, location) => {
+            if (
+                location &&
+                location.properties &&
+                location.properties.provider_brand
+            ) {
+                acc[location.properties.provider_brand] =
+                    acc[location.properties.provider_brand] || [];
+                acc[location.properties.provider_brand].push(location);
+            }
+            return acc;
+        }, {});
+
+        console.log(locationsByPharmacy);
+        return (
+            <div className={locations.length <= 0 ? "mb-0" : "mb-2"}>
+                <h3 className="font-weight-normal mb-0">
+                    <u>Realtime availability:</u>
+                </h3>
+                <p
+                    className="mb-2"
+                    style={{ lineHeight: "100%", marginTop: "4px" }}
+                >
+                    <small>
+                        Realtime availability is updated every minute.{" "}
+                        <Link href="/about-us#where-does-real-time-availability-information-come-from-">
+                            Learn more
+                        </Link>
+                    </small>
+                </p>
+                <div className="d-flex flex-row">
+                    <div>
+                        <ImSpinner2 className="rotating" />
+                    </div>
+                    <div className="ml-1" style={{ fontSize: "110%" }}>
+                        Last {locations.length <= 0 ? "checked" : "updated"}{" "}
+                        {moment(lastUpdated).format("h:mma")}
+                    </div>
+                </div>
+                {Object.keys(locationsByPharmacy).map((providerId) => {
+                    const locations = locationsByPharmacy[providerId];
+                    if (!locations || locations.length === 0) {
+                        return null;
+                    }
+                    return (
+                        <div key={providerId} className="my-3">
+                            <RealtimeLocationCard
+                                // location={locationsByPharmacy[providerId][0]}
+                                providerId={providerId}
+                                locations={locations}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
 }
