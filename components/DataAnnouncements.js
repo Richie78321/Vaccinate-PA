d2e94a13899053605a9571da25c967f1cebb89af
Richie Goulazian <rgoulazian@gmail.com>
2021-04-20 11:30:21 -0400

    Remove volunteer advert at top of data pages

--- components/DataAnnouncements.js
+++ components/DataAnnouncements.js
@@ -3,9 +3,9 @@ import { InlineShareButtons } from "sharethis-reactjs";
 export default function DataAnnouncements({ sharethisConfig }) {
   return (
     <>
-      <p className="text-center">
+      {/* <p className="text-center">
         <a href="https://forms.gle/5vyDk2tTjYUTMTXu6">Volunteer with us!</a>
-      </p>
+      </p> */}
       <div className="mb-3">
         <InlineShareButtons config={sharethisConfig} />
       </div>
