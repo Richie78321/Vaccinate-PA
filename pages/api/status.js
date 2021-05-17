21e2cb1bd225fd10a4412aa4ea0e3c8fd29dcb35
Richie78321 <Richie78321@users.noreply.github.com>
2021-05-17 00:41:22 +0000

    Apply Prettier formatting changes

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
   res.json({
-    dataSource: process.env.ARCHIVE_MODE === "true" ? "Google Sheets" : "Airtable",
-  })
+    dataSource:
+      process.env.ARCHIVE_MODE === "true" ? "Google Sheets" : "Airtable",
+  });
 }
